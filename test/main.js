dd30b546ee9699a25bde2b9e23d5ba9086e9f598
Roy Gitahi <roygitahi0@gmail.com>
2026-04-01 00:36:36 +0300

    add:new file

--- test/main.js
+++ test/main.js
@@ -1,91 +1,91 @@
-// Day temperature data
-let day1TempF = 32;
-let day2TempC = 25;
-let day3TempF = 70;
-let day4TempC = 18;
-let day5TempF = 80;
-let day6TempC = 15;
-let day7TempF = 72;
-let day8TempC = 28;
-let day9TempF = 68;
-let day10TempC = 20;
-let day11TempF = 75;
-let day12TempC = 23;
-let day13TempF = 82;
-let day14TempC = 30;
-let day15TempF = 65;
-let day16TempC = 22;
-let day17TempF = 77;
-let day18TempC = 26;
-let day19TempF = 78;
-let day20TempC = 24;
-let day21TempF = 73;
-let day22TempC = 21;
-let day23TempF = 79;
-let day24TempC = 27;
-let day25TempF = 71;
-let day26TempC = 19;
-let day27TempF = 74;
-let day28TempC = 17;
-let day29TempF = 76;
-let day30TempC = 29;
+// Fahrenheit
+const day1TempF = 32;
+const day3TempF = 70;
+const day5TempF = 80;
+const day7TempF = 72;
+const day9TempF = 68;
+const day11TempF = 75;
+const day13TempF = 82;
+const day15TempF = 65;
+const day17TempF = 77;
+const day19TempF = 78;
+const day21TempF = 73;
+const day23TempF = 79;
+const day25TempF = 71;
+const day27TempF = 74;
+const day29TempF = 76;
 
-// Convert all Celsius days to Fahrenheit
-let day2TempF = (day2TempC * 9 / 5) + 32;
-let day4TempF = (day4TempC * 9 / 5) + 32;
-let day6TempF = (day6TempC * 9 / 5) + 32;
-let day8TempF = (day8TempC * 9 / 5) + 32;
-let day10TempF = (day10TempC * 9 / 5) + 32;
-let day12TempF = (day12TempC * 9 / 5) + 32;
-let day14TempF = (day14TempC * 9 / 5) + 32;
-let day16TempF = (day16TempC * 9 / 5) + 32;
-let day18TempF = (day18TempC * 9 / 5) + 32;
-let day20TempF = (day20TempC * 9 / 5) + 32;
-let day22TempF = (day22TempC * 9 / 5) + 32;
-let day24TempF = (day24TempC * 9 / 5) + 32;
-let day26TempF = (day26TempC * 9 / 5) + 32;
-let day28TempF = (day28TempC * 9 / 5) + 32;
-let day30TempF = (day30TempC * 9 / 5) + 32;
+// Celsius
+const day2TempC = 25;
+const day4TempC = 18;
+const day6TempC = 15;
+const day8TempC = 28;
+const day10TempC = 20;
+const day12TempC = 23;
+const day14TempC = 30;
+const day16TempC = 22;
+const day18TempC = 26;
+const day20TempC = 24;
+const day22TempC = 21;
+const day24TempC = 27;
+const day26TempC = 19;
+const day28TempC = 17;
+const day30TempC = 29;
 
-// Convert all Fahrenheit days to Celsius
-let day1TempC = (day1TempF - 32) * 5 / 9;
-let day3TempC = (day3TempF - 32) * 5 / 9;
-let day5TempC = (day5TempF - 32) * 5 / 9;
-let day7TempC = (day7TempF - 32) * 5 / 9;
-let day9TempC = (day9TempF - 32) * 5 / 9;
-let day11TempC = (day11TempF - 32) * 5 / 9;
-let day13TempC = (day13TempF - 32) * 5 / 9;
-let day15TempC = (day15TempF - 32) * 5 / 9;
-let day17TempC = (day17TempF - 32) * 5 / 9;
-let day19TempC = (day19TempF - 32) * 5 / 9;
-let day21TempC = (day21TempF - 32) * 5 / 9;
-let day23TempC = (day23TempF - 32) * 5 / 9;
-let day25TempC = (day25TempF - 32) * 5 / 9;
-let day27TempC = (day27TempF - 32) * 5 / 9;
-let day29TempC = (day29TempF - 32) * 5 / 9;
+// Convert F → C
+const day1C = (day1TempF - 32) * 5 / 9;
+const day3C = (day3TempF - 32) * 5 / 9;
+const day5C = (day5TempF - 32) * 5 / 9;
+const day7C = (day7TempF - 32) * 5 / 9;
+const day9C = (day9TempF - 32) * 5 / 9;
+const day11C = (day11TempF - 32) * 5 / 9;
+const day13C = (day13TempF - 32) * 5 / 9;
+const day15C = (day15TempF - 32) * 5 / 9;
+const day17C = (day17TempF - 32) * 5 / 9;
+const day19C = (day19TempF - 32) * 5 / 9;
+const day21C = (day21TempF - 32) * 5 / 9;
+const day23C = (day23TempF - 32) * 5 / 9;
+const day25C = (day25TempF - 32) * 5 / 9;
+const day27C = (day27TempF - 32) * 5 / 9;
+const day29C = (day29TempF - 32) * 5 / 9;
 
-// Total temperatures
-let tot_temperature_in_fahrenheit =
-  day1TempF + day2TempF + day3TempF + day4TempF + day5TempF +
-  day6TempF + day7TempF + day8TempF + day9TempF + day10TempF +
-  day11TempF + day12TempF + day13TempF + day14TempF + day15TempF +
-  day16TempF + day17TempF + day18TempF + day19TempF + day20TempF +
-  day21TempF + day22TempF + day23TempF + day24TempF + day25TempF +
-  day26TempF + day27TempF + day28TempF + day29TempF + day30TempF;
+// Convert C → F
+const day2F = (day2TempC * 9 / 5) + 32;
+const day4F = (day4TempC * 9 / 5) + 32;
+const day6F = (day6TempC * 9 / 5) + 32;
+const day8F = (day8TempC * 9 / 5) + 32;
+const day10F = (day10TempC * 9 / 5) + 32;
+const day12F = (day12TempC * 9 / 5) + 32;
+const day14F = (day14TempC * 9 / 5) + 32;
+const day16F = (day16TempC * 9 / 5) + 32;
+const day18F = (day18TempC * 9 / 5) + 32;
+const day20F = (day20TempC * 9 / 5) + 32;
+const day22F = (day22TempC * 9 / 5) + 32;
+const day24F = (day24TempC * 9 / 5) + 32;
+const day26F = (day26TempC * 9 / 5) + 32;
+const day28F = (day28TempC * 9 / 5) + 32;
+const day30F = (day30TempC * 9 / 5) + 32;
 
-let tot_temperature_in_celsius =
-  day1TempC + day2TempC + day3TempC + day4TempC + day5TempC +
-  day6TempC + day7TempC + day8TempC + day9TempC + day10TempC +
-  day11TempC + day12TempC + day13TempC + day14TempC + day15TempC +
-  day16TempC + day17TempC + day18TempC + day19TempC + day20TempC +
-  day21TempC + day22TempC + day23TempC + day24TempC + day25TempC +
-  day26TempC + day27TempC + day28TempC + day29TempC + day30TempC;
+const tot_temperature_in_celsius =
+  day1C + day2TempC + day3C + day4TempC + day5C + day6TempC +
+  day7C + day8TempC + day9C + day10TempC + day11C + day12TempC +
+  day13C + day14TempC + day15C + day16TempC + day17C + day18TempC +
+  day19C + day20TempC + day21C + day22TempC + day23C + day24TempC +
+  day25C + day26TempC + day27C + day28TempC + day29C + day30TempC;
 
-// Average temperatures
-let avg_temperature_in_fahrenheit = tot_temperature_in_fahrenheit / 30;
-let avg_temperature_in_celsius = tot_temperature_in_celsius / 30;
+const tot_temperature_in_fahrenheit =
+  day1TempF + day2F + day3TempF + day4F + day5TempF + day6F +
+  day7TempF + day8F + day9TempF + day10F + day11TempF + day12F +
+  day13TempF + day14F + day15TempF + day16F + day17TempF + day18F +
+  day19TempF + day20F + day21TempF + day22F + day23TempF + day24F +
+  day25TempF + day26F + day27TempF + day28F + day29TempF + day30F;
 
-console.log("Total in °F:", tot_temperature_in_fahrenheit);
-console.log("Total in °C:", tot_temperature_in_celsius);
-console.log("Average in °F:", avg_temperature_in_fahrenheit);
-console.log("Average in °C:", avg_temperature_in_celsius);
+const avg_temperature_in_celsius = tot_temperature_in_celsius / 30;
+const avg_temperature_in_fahrenheit = tot_temperature_in_fahrenheit / 30;
+
+module.exports = {
+  tot_temperature_in_fahrenheit,
+  tot_temperature_in_celsius,
+  avg_temperature_in_fahrenheit,
+  avg_temperature_in_celsius
+};
